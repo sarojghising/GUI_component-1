078f62258e1b7f601df8a633786aa2f9ac3f6600
saroj <sarojghising13@gmail.com>
2020-12-04 21:45:39 +0545

    validated textfield name and parameter

---
+++ GPL_Component1/BaseParameterValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPL_Component1
+{
+    class BaseParameterValidation
+    {
+        public Boolean validate(string[] data)
+        {
+            return data.Length >= 2;
+        }
+
+
+
+        public void errorMessage()
+        {
+            System.Windows.Forms.MessageBox.Show("Invalid parameters passed. Three or more arguments required to pass the test");
+        }
+    }
+}
