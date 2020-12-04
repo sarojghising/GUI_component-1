e234bd7b93cdb7a391241153b300dd2435d1c1df
saroj <sarojghising13@gmail.com>
2020-12-04 22:03:05 +0545

    defined necessary methods to draw shape

---
+++ GPL_Component1/ShapeInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPL_Component1
+{
+    interface ShapeInterface
+    {       
+            void set(System.Drawing.Color c, params int[] list);
+            void draw(Graphics g);
+            double calcArea();
+            double calcPerimeter();
+
+    }
+}
