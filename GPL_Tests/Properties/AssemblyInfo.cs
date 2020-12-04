fdbba303195cb3d8b77b88e1acd12253ff11b47f
saroj <sarojghising13@gmail.com>
2020-12-04 22:14:52 +0545

    unit testing and setup file of C# components

---
+++ GPL_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GPL_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GPL_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7ed290cb-ccbc-484c-9f47-5cb73fb6492d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GPL_Tests/ShapesValidationTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GPL_Component1;
+namespace GPL_Tests
+{
+    [TestClass]
+    public class ShapesValidationTest
+    {
+        private ShapesValidator sv;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            
+            this.sv = new ShapesValidator();
+        }
+    
+        [TestMethod]
+        public void TestValidCirclePasses()
+        {
+
+            bool isCircle = this.sv.validate("circle");    
+            Assert.AreEqual(isCircle, true);
+
+        }
+        [TestMethod]
+        public void TestInvalidShapeFails()
+        {
+            
+            bool invalid = this.sv.validate("space");
+            Assert.AreEqual(invalid, false);
+        }
+    }
+}
