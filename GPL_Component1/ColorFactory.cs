ba6f5f10be731798f77440a2632acf2a3210b942
saroj <sarojghising13@gmail.com>
2020-12-04 21:52:02 +0545

    get different type of color

---
+++ GPL_Component1/ColorFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace GPL_Component1
+{
+    class ColorFactory
+    {
+        public Color GetColor(string name)
+        {
+            string checkColor = name.ToLower().Trim();
+            switch (checkColor)
+            {
+                case "red":
+                    return Color.Red;
+                case "green":
+                    return Color.Green;
+                case "yellow":
+                    return Color.Yellow;
+                case "orange":
+                    return Color.Orange;
+                case "black":
+                    return Color.Black;
+                default:
+                    return Color.White;
+            }
+
+        }
+    }
+}
